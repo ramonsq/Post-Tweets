652d900fd889af5d13ebb3d1f3c1cd1701ad9e14
Ramón Sánchez Quijada <46969560+ramonsq@users.noreply.github.com>
2023-02-07 13:18:12 +0100

    Add files via upload

---
+++ Practica5.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+# Devuelve informacion del usuario que le meta
+url = "https://api.twitter.com/2/tweets/search/recent?query=ramonsmitsaky"
+
+payload={}
+headers = {
+  'Cookie': 'guest_id=v1%3A167269726562318385'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+# Mandamos un tweet
+url = "https://api.twitter.com/2/tweets"
+
+payload = json.dumps({
+  "text": "Hello World! este es mi tercer tweet"
+})
+headers = {
+  'Authorization': '', #aqui cada uno pone el suyo
+  'Content-Type': 'application/json',
+  'Cookie': 'guest_id=v1%3A167269726562318385'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+
+
+
+
+
